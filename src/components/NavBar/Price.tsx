d05f9b8c27690ccf26052b543b68b7d8c6c314b7
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-11-14 20:34:24 +0530

    fix: random price sort

--- src/components/NavBar/Price.tsx
+++ src/components/NavBar/Price.tsx
@@ -9,10 +9,10 @@ export default function Price() {
   return (
     <div>
       <label>Sort by price:</label>
-      <select onChange={handlePrice} className="bg-gray-300 rounded-md p-2">
-        <option value="rand">Random</option>
-        <option value="increasing">Increasing</option>
-        <option value="decreasing">Decreasing</option>
+             <select onChange={handlePrice} className="bg-gray-300 rounded-md p-2">
+                <option value="random">Random</option>
+               <option value="increasing">Increasing</option>
+               <option value="decreasing">Decreasing</option>
       </select>
     </div>
   );
